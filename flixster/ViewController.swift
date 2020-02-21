a277162a987f7a35cf9115c8a2c3fcd70987b867
Qingzhi Luo <tomrony97@outlook.com>
2020-02-20 20:05:09 -0800

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Qingzhi Luo on 2/20/20.
+//  Copyright © 2020 Qingzhi Luo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
